4066f9f218fd9a34010a137bee11bb0f9bd8f6d5
o0173 <diana.zinchenko@hneu.net>
2024-01-11 16:28:17 +0200

    Initial commit

---
+++ src/main/java/edu/hneu/mjt/zinchenko/ticket_1_26/ExaminationTicket126Application.java
@@ -0,0 +1,13 @@
+package edu.hneu.mjt.zinchenko.ticket_1_26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExaminationTicket126Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExaminationTicket126Application.class, args);
+    }
+
+}
---
+++ src/test/java/edu/hneu/mjt/zinchenko/ticket_1_26/ExaminationTicket126ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.hneu.mjt.zinchenko.ticket_1_26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExaminationTicket126ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
