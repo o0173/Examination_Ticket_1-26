3f62c23a3aafc238d5f865e3423223cb7074a10f
o0173 <diana.zinchenko@hneu.net>
2024-01-11 16:50:49 +0200

    Added CRUD operations with SoftwareProduct entity

--- src/main/java/edu/hneu/mjt/zinchenko/ticket_1_26/repository/DeveloperRepository.java
+++ src/main/java/edu/hneu/mjt/zinchenko/ticket_1_26/repository/DeveloperRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface DeveloperRepository extends JpaRepository<Developer, Long> {
-
+    @Query("SELECT d.name, COUNT(sp.id) FROM Developer d LEFT JOIN SoftwareProduct sp ON d.id = sp.developer.id GROUP BY d.name")
+    List<Object[]> findDeveloperProductCount();
 }
